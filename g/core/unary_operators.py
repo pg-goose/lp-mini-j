02a273f80d87dc01c78cd6b926da0274ddd4cd00
pg-goose <pau.galopa.dev@gmail.com>
2025-05-18 02:55:55 +0200

    added support for more unary expressions

--- g/core/unary_operators.py
+++ g/core/unary_operators.py
@@ -1,3 +1,6 @@
 UNARYOPS_SWITCH = {
     '_': lambda x: -x,
+    'i.': lambda x: list(range(x)), # TODO investigate if we can leave only range for lazy evaluation
+    '#': lambda x: len(x),
+    ']': lambda x: x,
 }
