ad9e755537b4bc45fd87c29eaa610a12fe37ffe7
pg-goose <pau.galopa.dev@gmail.com>
2025-06-01 19:19:14 +0200

    print '_' when negative number found

--- g/core/result.py
+++ g/core/result.py
@@ -1,16 +1,16 @@
 from g.core.error import GError
 
-
 class Result:
-    def __init__(self, value):
+    def __init__(self, value: any | GError):
         self.value = value
 
     def __repr__(self):
+        def fmt(v):
+            if isinstance(self.value, int):
+                return f'_{abs(v)}'
+            return f'{v}'
         if isinstance(self.value, list):
-            return f" ".join([str(v) for v in self.value])
-        return str(self.value)
-    
+            return ' '.join(fmt(v) for v in self.value)
+
     def __str__(self):
-        if isinstance(self.value, list):
-            return f" ".join([str(v) for v in self.value])
-        return str(self.value)
+        return self.__repr__()
